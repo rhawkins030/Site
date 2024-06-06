97ace86324948e11314f6b9b2394914dbd097f8e
Chris <chrizxzftw@gmail.com>
2024-06-06 16:46:16 -0400

    Additions and Removals
    - Removed the old lunes site
    - Added the new lunes site

--- javascript/menu-mobile.js
+++
@@ -1,9 +0,0 @@
-    
-    
-    
-    $(document).ready(function() {
-        $('.nav-toggle').click(function() {
-        $('.nav-links').slideToggle();
-        $('.nav-toggle').toggleClass('open');
-        });
-    });
